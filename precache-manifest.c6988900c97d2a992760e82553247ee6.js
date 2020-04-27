a67a7ca03a7837c988b06750d340181bc73bdbea
Adair Daniels <adairdaniels@gmail.com>
2020-04-26 21:45:51 -0700

    Updates

---
+++ precache-manifest.c6988900c97d2a992760e82553247ee6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63089e5e4b4e5d39db95974ade4fd7eb",
+    "url": "/stocktwitsapi/index.html"
+  },
+  {
+    "revision": "434af14725ac1a9d1acf",
+    "url": "/stocktwitsapi/static/css/main.cddd883a.chunk.css"
+  },
+  {
+    "revision": "6f3943a8768e84649599",
+    "url": "/stocktwitsapi/static/js/2.e2e7b589.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/stocktwitsapi/static/js/2.e2e7b589.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "434af14725ac1a9d1acf",
+    "url": "/stocktwitsapi/static/js/main.49e1872c.chunk.js"
+  },
+  {
+    "revision": "9f7e5d7590f295eacfc7",
+    "url": "/stocktwitsapi/static/js/runtime-main.e56d4fd0.js"
+  }
+]);
