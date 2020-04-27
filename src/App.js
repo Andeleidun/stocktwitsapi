fd2024cbce10b5c9aea3e6971ce283481290c5f2
Adair Daniels <adairdaniels@gmail.com>
2020-04-26 21:41:38 -0700

    Added auto-pull, styled.

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import TextField from '@material-ui/core/TextField';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import Chip from '@material-ui/core/Chip';
 import Badge from '@material-ui/core/Badge';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 class Header extends React.Component {
   render() {
@@ -36,10 +37,10 @@ class CardTemplate extends React.Component {
           <figure className="picture">
             <img src={this.props.message.user.avatar_url} />
           </figure>
-          <Typography variant="p" className="namearea">
+          <Typography variant="body1" className="namearea">
             <span className="name">{this.props.message.user.name}</span> <span className="username">@ {this.props.message.user.username}</span>
           </Typography>
-          <Typography variant="p" className="text">
+          <Typography variant="body1" className="text">
             {this.props.message.body}
           </Typography>
         </CardContent>
@@ -54,40 +55,73 @@ class App extends React.Component {
     this.state = {
       symbols: [],
       input: '',
+      error: '',
+      interval: undefined,
+      currentCount: 5,
       loading: false
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.timer = this.timer.bind(this);
   }
 
   chips = [];
   tweets = [];
+  content = [];
 
   renderChips(symbols) {
     let chips = [];
     for (let symbol of symbols) {
-      chips.push(
-        <Badge badgeContent={symbol.tweets.length} className="badge">
-          <Chip
-            label={symbol.label}
-            className="chip" 
-          />
-        </Badge>
-      )
+      if (symbol.tweets) {
+        chips.push(
+          <Badge badgeContent={symbol.tweets.length} className="badge">
+            <Chip
+              label={symbol.label}
+              className="chip" 
+            />
+          </Badge>
+        )
+      }
+      this.chips = chips;
     }
-    this.chips = chips;
+  }
+
+  setTimer() {
+    let intervalId = setInterval(this.timer, 60000);
+    this.setState({
+      interval: intervalId,
+      currentCount: 5
+    });
+  }
+
+  timer() {
+    let newCount = this.state.currentCount - 1;
+    if (newCount >= 0) {
+      this.setState({ currentCount: newCount })
+    } else {
+      this.submitRequest();
+      clearInterval(this.state.interval);
+      this.setTimer();
+    }
   }
 
   renderTweets(symbols) {
     let tweets = [];
+    let tweetsFound = 0;
     for (let symbol of symbols) {
-      for (let tweet of symbol.tweets)
-      tweets.push(
-        <CardTemplate
-          classGiven="card"
-          message={tweet}
-        />
-      );
+      if (symbol.tweets) {
+        tweetsFound++;
+        for (let tweet of symbol.tweets)
+        tweets.push(
+          <CardTemplate
+            classGiven="card"
+            message={tweet}
+          />
+        );
+      }
+    }
+    if (tweetsFound > 0) {
+      this.setTimer();
     }
     this.tweets = tweets;
   }
@@ -107,7 +141,7 @@ class App extends React.Component {
       },
     })
     .then(res => res.json())
-    .catch(error => console.error(error))
+    .catch(error => this.setState({ error: error}));
     for (let symbol of symbols) {
       symbol.url = urlBase.concat(symbol.label).concat(urlEnd);
       let finalUrl = proxyUrl.concat(symbol.url);
@@ -115,20 +149,17 @@ class App extends React.Component {
         .then((data) => {
           symbol.tweets = data.messages;
         })
-        .catch(error => console.error(error));
-      console.log(symbol);
+        .catch(error => this.setState({ error: error}));
     }
-    this.renderChips(symbols);
-    this.renderTweets(symbols);
+    if (!this.state.error) {
+      this.renderChips(symbols);
+      this.renderTweets(symbols);
+    }
     this.setState({ loading: false });
     return(symbols);
   }
 
-  handleChange(event) {
-    this.setState({input: event.target.value});
-  }
-
-  async handleSubmit(event) {
+  async submitRequest() {
     const newSymbols = this.state.input.toUpperCase().replace(/\s+/g, '').split(",");
     let formattedSymbols = [];
     let key = 0;
@@ -140,10 +171,34 @@ class App extends React.Component {
     }
     formattedSymbols = await this.retrieveTweets(formattedSymbols);
     this.setState({symbols: formattedSymbols});
+  }
+
+  handleChange(event) {
+    this.setState({input: event.target.value});
+  }
+
+  async handleSubmit(event) {
+    this.submitRequest();
     event.preventDefault();
   }
 
   render() {
+    if (this.state.loading) {
+      this.content = <CircularProgress />;
+    } else if (this.state.error) {
+      this.content = <p>{this.state.error}</p>
+    } else {
+      this.content = (
+        <section className="content">
+          <section className="chips">
+            {this.chips}
+          </section>
+          <section className="tweets">
+            {this.tweets}
+          </section>
+        </section>
+      );
+    }
     return (
       <div className="App">
         <Header />
@@ -153,7 +208,7 @@ class App extends React.Component {
               <TextField
                 className="stock-input"
                 id="stock-symbols"
-                label="Input stock symbols"
+                label="Input stock symbols (separate with a comma)"
                 value={this.state.input}
                 onChange={this.handleChange}
                 InputProps={{
@@ -168,12 +223,7 @@ class App extends React.Component {
               <Button className="search-button" variant="contained" onClick={this.handleSubmit}>Search</Button>
             </form>
           </section>
-          <section className="chips">
-            {this.chips}
-          </section>
-          <section className="tweets">
-            {this.tweets}
-          </section>
+          {this.content}
         </main>
       </div>
     );
